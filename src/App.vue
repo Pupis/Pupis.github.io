ef22f1597a0aa2c481f0d6c16b842505bcf2a0c5
Justas Pupelis <j.pupelis@adform.com>
2018-11-05 23:11:24 +0200

    Something to the right direction

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>Justas Pupelis</h1>
+        <p>Software engineer</p>
+      </v-flex>
+      <v-flex xs12>
+        <v-layout row justify-center>
+            <v-flex xs1 v-for="icon in icons" :key="icon.name" class="mx-3">
+              <v-btn flat icon :href="icon.url" :target="icon.target">
+                <v-icon>{{ icon.name }}</v-icon>
+              </v-btn>
+            </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data: () => ({
+    icons: [{ name: 'mdi-linkedin-box', url: 'https://lt.linkedin.com/in/justas-pupelis-76797545', target: '_blank' },
+      { name: 'mdi-github-box', url: 'https://github.com/Pupis', target: '_blank' },
+      { name: 'mdi-facebook-box', url: 'https://www.facebook.com/pupelis', target: '_blank' },
+      { name: 'mdi-instagram', url: 'https://www.instagram.com/pupelis/', target: '_blank' },
+    ],
+  }),
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, { iconfont: 'mdi' });
 
 /* eslint-disable no-new */
 new Vue({
